cb4951ddeeb8a2e559e6c20adf702583894f9423
Alain <alain.riou@ens-paris-saclay.fr>
2019-06-12 16:10:33 +0200

    use glob

--- util/ply_to_pk.py
+++ util/ply_to_pk.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-import pickle
+import glob
 import random
 import sys
 import time
@@ -9,15 +9,13 @@ from tqdm import tqdm
 import torch
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SOURCE_PATH = '/storage/group/hodl4cv/lopc/driving_project/episode_000'
-DEST_PATH = '../datasets/Carla/episode_000'
-
-N_POINTS = 2**16
+SOURCE_PATH = '/storage/group/hodl4cv/lopc/driving_project'
+DEST_PATH = '../datasets/Carla'
 
 def _distances(p0, pts, d=3):
     return torch.norm((p0.unsqueeze(0)-pts)[:,:d], dim=1)
 
-def fps(data, npoint):
+def fps(data, npoint, first=None):
     r"""
         Farthest point sampling (greedy algorithm)
 
@@ -27,6 +25,8 @@ def fps(data, npoint):
             (N,d) tensor containing point cloud
         npoint: int
             number of points that have to be selected
+        first: int (optional)
+            index of first point selected (random if first is None)
 
         Returns
         -------
@@ -38,9 +38,9 @@ def fps(data, npoint):
     if N < npoint:
         return data
     farthest_pts = torch.randn(npoint, d, dtype=torch.double).cuda()
-    farthest_pts[0] = data[random.randint(0,N-1)]
+    farthest_pts[0] = data[random.randint(0,N-1)] if first is None else data[first]
     distances = _distances(farthest_pts[0], data, d=min(d,3))
-    for i in tqdm(range(1,npoint)):
+    for i in range(1,npoint):
         pt = data[distances.argmax()]
         distances = torch.min(distances, _distances(pt, data, d=min(d,3)))
         farthest_pts[i] = pt
@@ -59,7 +59,7 @@ def convert(source, dest, replace):
     """
     if not replace and os.path.isfile(dest):
         return 0
-    print(' > Converting %s...' % source, end='\t')
+    print(' > Converting %s...' % source)
     pc = np.loadtxt(source, skiprows=10, delimiter=' ', dtype=float)
     data = torch.from_numpy(pc)
     start = time.time()
@@ -70,25 +70,9 @@ def convert(source, dest, replace):
     end = time.time()
     print('Done. Time elpased: %.3f' % (end-start))
 
-def explore(source, dest, replace):
-    '''
-    Explores recursively [source] and converts every .ply file in it to a binary
-    file. Binary files are stocked in [dest], with the same architecture as in
-    [source]
-    '''
-    print('Exploring %s...' % source)
-    for file in os.listdir(source):
-        dir = os.path.join(source, file)
-        if os.path.isdir(dir):
-            if len(file) == 11 and 'PointCloud' in file: # do not convert global point clouds
-                continue
-            explore(dir, os.path.join(dest, file), replace)
-        elif len(file) > 5 and file[-5:] == '%s.ply' % sys.argv[1]: # partitionning the dataset to launch script on multiple machines
-            os.makedirs(dest, exist_ok=True)
-            new_file = '%s.pickle' % file[:-4]
-            convert(os.path.join(source, file), os.path.join(dest, new_file), replace)
-
 if __name__ == '__main__':
-    replace = False
-    explore(SOURCE_PATH, os.path.join(BASE_DIR, DEST_PATH), replace)
-    print('Done')
+    print('Conversion of local point clouds from %s...' % SOURCE_PATH)
+    os.chdir(SOURCE_PATH)
+    for file in glob.iglob(os.path.join('./**/*.ply'), recursive=True):
+        new_file = os.path.join(DEST_PATH, file[:-3] + 'npy')
+        convert(file, new_file, False)
