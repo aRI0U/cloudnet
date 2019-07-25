b7ae9014628f8a9a631a7d5cbbc22c05e0e6c829
Alain Riou <alain.riou@ens-paris-saclay.fr>
2019-07-25 12:15:13 +0200

    change loading driving data

--- data/cloudnet_dataset.py
+++ data/cloudnet_dataset.py
@@ -1,57 +1,47 @@
-import csv
-from numpy import load
+import numpy as np
 import os
 
 import torch
 from torch.utils.data import Dataset
 
-from util.geometry import euler_to_quaternion
-
 class CloudNetDataset(Dataset):
     def initialize(self, opt):
         self.opt = opt
         self.root = opt.dataroot
-        driving_data = os.path.join(self.root, 'driving_data.csv')
+        driving_data = os.path.join(self.root, 'poses.txt')
         # select the folder corresponding to the right input type
-        file_path = os.path.join('PointCloudLocal1', 'point_cloud_%s.npy')
-        self.mean_image = None #np.load(os.path.join(self.root , 'mean_image.npy'))
-        self.A_paths = []
-        self.A_poses = []
+        pc_path = os.path.join(self.root, 'PointCloudLocal1', 'point_cloud_%05d.npy')
+        self.mean_image = np.load(os.path.join(self.root , 'mean_image.npy'))
+        frames = np.loadtxt(driving_data, dtype=int, usecols=0, delimiter=';', skiprows=1)
+        poses = np.loadtxt(driving_data, dtype=float, usecols=(1,2,3,4,5,6,7), delimiter=';', skiprows=1)
 
-        # extract driving data
-        print('Extracting data from %s...' % driving_data, end='\t')
-        with open(driving_data, 'r') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                img_number = row['name'][-5:]
-                if opt.split > 0:
-                    if (opt.isTrain or opt.phase == 'retrain') and int(img_number) % opt.split == 0:
-                        continue
-                    if opt.phase == 'val' and int(img_number) % opt.split != 0:
-                        continue
-                path = os.path.join(self.root, file_path % img_number)
-                # ignore the line if the point cloud doesn't exist
-                if not os.path.isfile(path):
-                    continue
-                self.A_paths.append(path)
-                pose = torch.tensor((float(row['pos_x']), float(row['pos_y']), float(row['pos_z'])))
-                # for now only 1 dof of rotation is enabled
-                pose = torch.cat((pose, euler_to_quaternion(0, 0, float(row['steer'])/2)))
-                self.A_poses.append(pose)
-        print('Done')
-        self.A_size = len(self.A_paths)
+        # splitting between training and test sets
+        set = np.ones(len(frames), dtype=bool)
+        if opt.split > 0:
+            if opt.isTrain or opt.phase == 'retrain':
+                set = frames % opt.split != 0
+
+            elif opt.phase == 'val':
+                set = frames % opt.split == 0
+
+        frames = frames[set]
+        self.pc_paths = [pc_path % f for f in frames]
+
+        self.poses = poses[set]
+
+        self.size = len(self.pc_paths)
 
     def __getitem__(self, index):
-        index_A = index % self.A_size
-        A_path = self.A_paths[index % self.A_size]
-        A_pose = self.A_poses[index % self.A_size]
+        index_A = index % self.size
+        pc_path = self.pc_paths[index_A]
+        pose = self.poses[index_A]
 
-        A = self._extract_file(A_path)
+        pc = self._extract_file(pc_path)
 
-        return {'X': A, 'Y': A_pose, 'X_paths': A_path}
+        return {'X': pc, 'Y': pose, 'X_path': pc_path}
 
     def __len__(self):
-        return self.A_size
+        return self.size
 
     def name(self):
         return 'CloudNetDataset'
@@ -77,8 +67,8 @@ class CloudNetDataset(Dataset):
     @staticmethod
     def _sample(path, input_nc, n_points, sampling):
         if sampling == 'fps':
-            return load(path, mmap_mode='r')[:n_points, :input_nc]
+            return np.load(path, mmap_mode='r')[:n_points, :input_nc]
         if sampling == 'uni':
-            data = load(path, mmap_mode='r')
+            data = np.load(path, mmap_mode='r')
             return data[::(len(data))//n_points][:n_points, :input_nc]
         raise ValueError('Sampling [%s] does not exist' % sampling)
